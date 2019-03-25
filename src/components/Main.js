be538d89b469973be4e67abfbbe716250b0b1398
HugGtz <arnoldohugo@gmail.com>
2019-03-24 19:07:23 -0600

    add component main

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+class Main extends Component {
+  render() {
+    return (
+        <Button type="danger">Danger</Button>
+    )
+  }
+}
+
+export default Main; // Don’t forget to use export default!
