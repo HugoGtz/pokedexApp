59a2603f970f9237546cab12db92abf98f71e195
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-29 18:58:28 +0000

    Deploy HugoGtz/pokedexApp to github.com/HugoGtz/pokedexApp.git:gh-pages

---
+++ precache-manifest.d2bb135bdff8211d8997fb7382821acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedexApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "32d2a8523bf9897f2d30",
+    "url": "/pokedexApp/static/js/runtime~main.5e185b8a.js"
+  },
+  {
+    "revision": "9d0d6f1d0e4f6c1dc0a4",
+    "url": "/pokedexApp/static/js/main.f2dc8c11.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/pokedexApp/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "9d0d6f1d0e4f6c1dc0a4",
+    "url": "/pokedexApp/static/css/main.6dc38653.chunk.css"
+  },
+  {
+    "revision": "aab362fae52378f9540ffd12d6c88d7f",
+    "url": "/pokedexApp/index.html"
+  }
+];
